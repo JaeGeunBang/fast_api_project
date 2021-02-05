275f4f54db71da121b7d42f7e8aefe913eef4d16
jaegeunbang <jgb710@naver.com>
2021-02-05 15:11:15 +0900

    Extra Data Type 추가

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
 ## fast_api_project
 
 - fastAPI 를 통해 웹페이지를 구성해보자 (fastAPI, uvicorn)
-- 아래 링크에서 하나씩 따라해볼것
-  - https://fastapi.tiangolo.com/tutorial/first-steps/
 
-- 완료 여부
-  - [x] first step 
-  - [x] path parameter
-  - [x] query parameter
-  - [x] request body
-  - [x] string validation
-  - [x] numeric validation
+- 아래 링크에서 하나씩 따라해볼것
+  
+- https://fastapi.tiangolo.com/tutorial/first-steps/
+  
+- [main.py](https://github.com/JaeGeunBang/fast_api_project/blob/main/main.py)
+
+  - first step 
+  - path parameter
+  - query parameter
+  - request body
+  - string validation
+  - numeric validation
+
+- [main2.py](https://github.com/JaeGeunBang/fast_api_project/blob/main/main2.py)
+
+  - Body - Multiple Parameter 
+
+  - Body - Field
+  - Body - Nested Models
+
+- [main3.py](https://github.com/JaeGeunBang/fast_api_project/blob/main/main3.py)
+
+  - Schema Extra - Example
+
+- 
+
+  
+
+  
--- main3.py
+++ main3.py
@@ -1,90 +1,54 @@
 from typing import Optional
 
-from fastapi import FastAPI, Path
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
-
+# Schema Extra - Example (default request body를 제공해줌)
 class Item(BaseModel):
     name: str
     description: Optional[str] = None
     price: float
     tax: Optional[float] = None
 
-class User(BaseModel):
-    username: str
-    full_name: Optional[str] = None
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+            }
+        }
 
-# Body - Multiple Parameters
 @app.put("/items/{item_id}")
-async def update_item(
-        *,
-        item_id: int = Path(..., title = "The ID of the item to get", gc=0, le=1000),
-        q: Optional[str] = None,
-        item: Optional[Item] = None,
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    if item:
-        results.update({"item": item})
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
     return results
 
-# Body - Multiple Parameters
-@app.put("/items2/{item_id}")
-async def update_item(
-        item_id: int,
-        item: Item,
-        user: User
-):
-    results = {"item_id": item_id, "item": item, "user": user}
-    return results
-
-# Body - Field
+# Field, example로 제공해줄수도 있음
 from pydantic import Field
-from fastapi import Body
 
 class Item2(BaseModel):
-    name: str
-    description: Optional[str] = Field (
-        None, Title="The description of the item", max_length=300
-    )
-    price: float = Field(..., gt=0, description="The price must be greater than zero")
-    tax: Optional[float] = None
+    name: str = Field(..., example="Foo")
+    description: Optional[str] = Field(None, example="A very nice Item")
+    price: float = Field(..., example=35.4)
+    tax: Optional[float] = Field(None, example=3.2)
 
-@app.put("/items2/{item_id}")
-async def update_item2(item_id: int, item: Item2 = Body(..., embed=True)):
-    results = {"item_id": item_id, "item": item}
+# 함수의 파라미터로 제공해줄수도 있음
+@app.put("/items/{item_id}")
+async def update_item(
+        item_id: int,
+        item: Item = Body(
+            ...,
+            example= {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+            },
+        ),
+):
+    result = {"item_id": item_id, "item": item}
     return results
-
-# Body - Nested Models
-from typing import List
-from pydantic import HttpUrl
-
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
-class Item3(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-    tags: List[str] = []
-    image: Optional[List[Image]] = None
-
-class Offer(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    items: List[Item]
-
-@app.put("/items3/{item_id}")
-async def update_item3(item_id: int, item: Item):
-    results = {"item_id": item_id, "item": item}
-    return results
-
-@app.post("/images/multiple/")
-async def create_multiple_images(images: List[Image]):
-    return images
