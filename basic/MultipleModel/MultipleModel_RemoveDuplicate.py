fa2c6220a88eb50ac23406f212c19348e21ecaa3
jaegeunbang <jgb710@naver.com>
2021-02-18 20:47:27 +0900

    MultipleModel 추가

---
+++ basic/MultipleModel/MultipleModel_RemoveDuplicate.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
+
+app = FastAPI()
+
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Optional[str] = None
+
+
+class UserIn(UserBase):
+    password: str
+
+
+class UserOut(UserBase):
+    pass
+
+
+class UserInDB(UserBase):
+    hashed_password: str
+
+
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
+
+# Field 이름이 매칭되지 않으면, 필드가 넘어가지 않는다.
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
+
+# 여기도 마찬가지.. user_saved 와 UserOut 사이 서로 매칭되는 필드만 노출된다.
+@app.post("/user/", response_model=UserOut)
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
